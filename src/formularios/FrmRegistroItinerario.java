98d195b806f99bbf8a7af09915e6ed539336f21f
Oscar Roman <oscar.roman229303@potros.itson.edu.mx>
2022-05-13 03:22:42 -0700

    Actualizado el FrmRegistroItinerario, falta funcionalidad.

--- src/formularios/FrmRegistroItinerario.java
+++ src/formularios/FrmRegistroItinerario.java
@@ -39,23 +39,23 @@ public class FrmRegistroItinerario extends javax.swing.JFrame {
         tablaZonas = new javax.swing.JTable();
         panelTablaGuias = new javax.swing.JScrollPane();
         tablaGuias = new javax.swing.JTable();
-        panelTablaZonasSeleccionadas = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
-        btnAgregarZona = new javax.swing.JButton();
-        btnEliminarZona = new javax.swing.JButton();
+        panelTablaDiasSemana = new javax.swing.JScrollPane();
+        tablaDiasSemana = new javax.swing.JTable();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         txtNombreItinerario = new javax.swing.JTextField();
         jLabel5 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        btnBuscar = new javax.swing.JButton();
         jLabel6 = new javax.swing.JLabel();
         txtDuracion = new javax.swing.JTextField();
         jLabel7 = new javax.swing.JLabel();
         txtLongitud = new javax.swing.JTextField();
         jLabel8 = new javax.swing.JLabel();
         txtVisitantes = new javax.swing.JTextField();
+        btnGuardarItinerario = new javax.swing.JButton();
+        btnRegresar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -64,12 +64,19 @@ public class FrmRegistroItinerario extends javax.swing.JFrame {
 
             },
             new String [] {
-                "Zonas registradas"
+                "Nombre", "Extension", "Seleccionar"
             }
         ) {
-            boolean[] canEdit = new boolean [] {
-                false
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.Double.class, java.lang.Object.class
             };
+            boolean[] canEdit = new boolean [] {
+                false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
                 return canEdit [columnIndex];
@@ -82,12 +89,19 @@ public class FrmRegistroItinerario extends javax.swing.JFrame {
 
             },
             new String [] {
-                "Guias registrados"
+                "Nombre", "Telefono"
             }
         ) {
-            boolean[] canEdit = new boolean [] {
-                false
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class
             };
+            boolean[] canEdit = new boolean [] {
+                false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
                 return canEdit [columnIndex];
@@ -95,31 +109,46 @@ public class FrmRegistroItinerario extends javax.swing.JFrame {
         });
         panelTablaGuias.setViewportView(tablaGuias);
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        tablaDiasSemana.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null}
+                {null, null}
             },
             new String [] {
-                "Zonas seleccionadas"
+                "Dia", "Seleccionar"
             }
-        ));
-        panelTablaZonasSeleccionadas.setViewportView(jTable1);
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.Object.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false
+            };
 
-        btnAgregarZona.setText("Agregar");
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
 
-        btnEliminarZona.setText("Eliminar");
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        panelTablaDiasSemana.setViewportView(tablaDiasSemana);
+        if (tablaDiasSemana.getColumnModel().getColumnCount() > 0) {
+            tablaDiasSemana.getColumnModel().getColumn(0).setResizable(false);
+            tablaDiasSemana.getColumnModel().getColumn(1).setResizable(false);
+        }
 
-        jLabel1.setText("Tabla zonas seleccionadas:");
+        jLabel1.setText("Días de la semana:");
 
-        jLabel2.setText("Tabla zonas disponibles:");
+        jLabel2.setText("Zonas disponibles:");
 
-        jLabel3.setText("Tabla guias");
+        jLabel3.setText("Guías disponibles:");
 
         jLabel4.setText("Nombre");
 
         jLabel5.setText("Duración (minutos):");
 
-        jButton1.setText("Buscar");
+        btnBuscar.setText("Buscar");
 
         jLabel6.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jLabel6.setText("Registro Itinerario");
@@ -127,6 +156,15 @@ public class FrmRegistroItinerario extends javax.swing.JFrame {
         jLabel7.setText("Longitud (metros):");
 
         jLabel8.setText("Visitantes máximos:");
+
+        btnGuardarItinerario.setText("Guardar itinerario");
+        btnGuardarItinerario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnGuardarItinerarioActionPerformed(evt);
+            }
+        });
+
+        btnRegresar.setText("Regresar");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -142,7 +180,7 @@ public class FrmRegistroItinerario extends javax.swing.JFrame {
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(txtNombreItinerario, javax.swing.GroupLayout.PREFERRED_SIZE, 167, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addComponent(btnBuscar, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(jLabel5)
@@ -152,30 +190,34 @@ public class FrmRegistroItinerario extends javax.swing.JFrame {
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                     .addComponent(txtDuracion)
                                     .addComponent(txtLongitud)
-                                    .addComponent(txtVisitantes, javax.swing.GroupLayout.DEFAULT_SIZE, 186, Short.MAX_VALUE))))
+                                    .addComponent(txtVisitantes, javax.swing.GroupLayout.DEFAULT_SIZE, 186, Short.MAX_VALUE)))
+                            .addComponent(btnGuardarItinerario))
                         .addGap(29, 29, 29)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel3)
                             .addComponent(jLabel2)
-                            .addComponent(jLabel1)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                .addComponent(btnEliminarZona, javax.swing.GroupLayout.Alignment.LEADING)
-                                .addComponent(btnAgregarZona, javax.swing.GroupLayout.Alignment.LEADING)
-                                .addComponent(panelTablaZonasSeleccionadas, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
-                                .addComponent(panelTablaGuias, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
-                                .addComponent(panelTablaZonas, javax.swing.GroupLayout.PREFERRED_SIZE, 325, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                .addComponent(btnRegresar)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel1)
+                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                        .addComponent(panelTablaDiasSemana, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                                        .addComponent(panelTablaGuias, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                                        .addComponent(panelTablaZonas, javax.swing.GroupLayout.PREFERRED_SIZE, 325, javax.swing.GroupLayout.PREFERRED_SIZE))))))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(291, 291, 291)
+                        .addGap(290, 290, 290)
                         .addComponent(jLabel6)))
                 .addContainerGap(42, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(28, 28, 28)
-                .addComponent(jLabel6)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 35, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGap(26, 26, 26)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel6)
+                    .addComponent(btnRegresar))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel3)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
@@ -183,19 +225,11 @@ public class FrmRegistroItinerario extends javax.swing.JFrame {
                         .addGap(8, 8, 8)
                         .addComponent(jLabel2)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(panelTablaZonas, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(btnAgregarZona)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jLabel1)
-                        .addGap(10, 10, 10)
-                        .addComponent(panelTablaZonasSeleccionadas, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(btnEliminarZona))
+                        .addComponent(panelTablaZonas, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(txtNombreItinerario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jButton1)
+                            .addComponent(btnBuscar)
                             .addComponent(jLabel4))
                         .addGap(28, 28, 28)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -208,12 +242,22 @@ public class FrmRegistroItinerario extends javax.swing.JFrame {
                         .addGap(24, 24, 24)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel8)
-                            .addComponent(txtVisitantes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addGap(20, 20, 20))
+                            .addComponent(txtVisitantes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnGuardarItinerario)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(panelTablaDiasSemana, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(50, 50, 50))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnGuardarItinerarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGuardarItinerarioActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnGuardarItinerarioActionPerformed
 
     
     private INegocio negocio;
@@ -221,9 +265,9 @@ public class FrmRegistroItinerario extends javax.swing.JFrame {
     private List<Zona> listaZonas;
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnAgregarZona;
-    private javax.swing.JButton btnEliminarZona;
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton btnBuscar;
+    private javax.swing.JButton btnGuardarItinerario;
+    private javax.swing.JButton btnRegresar;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -232,10 +276,10 @@ public class FrmRegistroItinerario extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
-    private javax.swing.JTable jTable1;
+    private javax.swing.JScrollPane panelTablaDiasSemana;
     private javax.swing.JScrollPane panelTablaGuias;
     private javax.swing.JScrollPane panelTablaZonas;
-    private javax.swing.JScrollPane panelTablaZonasSeleccionadas;
+    private javax.swing.JTable tablaDiasSemana;
     private javax.swing.JTable tablaGuias;
     private javax.swing.JTable tablaZonas;
     private javax.swing.JTextField txtDuracion;
